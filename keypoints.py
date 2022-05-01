144a112f3afe664c43a823c7f0eb78857ea03512
kanika637 <kanikagola26@gmail.com>
2022-05-01 21:55:41 +0530

    keypoints detection

--- keypoints.py
+++ keypoints.py
@@ -3,15 +3,22 @@ import cv2
 import time 
 import PoseModule as pm
 
-cap=cv2.VideoCapture("Abhay_Trik.mp4")
-img=cv2.imread("asan.jpg")
-detector=pm.poseDetector()
+cap=cv2.VideoCapture("Santosh_Trik.mp4")
+
+detector=pm.PoseDetector()
 
 while True:
-    # success, img= cap.read()
-    # img= cv2.resize(img,(1280,728))
-    img=detector.findPose(img)
-
+    success, img= cap.read()
+    img= cv2.resize(img,(1280,728))
+    # img=cv2.imread("asan.jpg")
+    img=detector.findPose(img,False)
+    lmlist=detector.getPosition(img,False)
+    # print(lmlist)
+    if len(lmlist) !=0:
+        #right arm
+        detector.findAngle(img,12,14,16)
+        #left arm
+        # detector.findAngle(img,11,13,15)
    
     cv2.imshow("Image", img)
     cv2.waitKey(1)
