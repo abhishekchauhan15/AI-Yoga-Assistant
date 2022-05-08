57d13fbda47b44a5ced296fd0b555def4aa82529
Jaideep2912 <jaideep2912@gmail.com>
2022-05-08 19:01:10 +0530

    Checking

--- client/src/Yoga.js
+++ client/src/Yoga.js
@@ -13,7 +13,42 @@ function Yoga() {
          <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
          <p className="btn"><FaPlayCircle/></p>
         </div>
+        <div className='card-2'>
+         <img className="asan-1" src={Vrksana} alt="Vrksana"/>
+         <h2 className='asan-name-1'>Vrksana Asana</h2>
+         <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
+         <p className="btn"><FaPlayCircle/></p>
+        </div>
+        <div className='card-3'>
+         <img className="asan-1" src={Vrksana} alt="Vrksana"/>
+         <h2 className='asan-name-1'>Vrksana Asana</h2>
+         <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
+         <p className="btn"><FaPlayCircle/></p>
+        </div>
     </div>
+
+
+    <div className='group-2'>
+        <div className='card-1'>
+         <img className="asan-1" src={Vrksana} alt="Vrksana"/>
+         <h2 className='asan-name-1'>Vrksana Asana</h2>
+         <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
+         <p className="btn"><FaPlayCircle/></p>
+        </div>
+        <div className='card-2'>
+         <img className="asan-1" src={Vrksana} alt="Vrksana"/>
+         <h2 className='asan-name-1'>Vrksana Asana</h2>
+         <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
+         <p className="btn"><FaPlayCircle/></p>
+        </div>
+        <div className='card-3'>
+         <img className="asan-1" src={Vrksana} alt="Vrksana"/>
+         <h2 className='asan-name-1'>Vrksana Asana</h2>
+         <p className='asan-para-1'>It is a good yoga jhsdgfkjhs gsfygsy gyutfuse tyurguygs ytruygisfyi yuyr</p>
+         <p className="btn"><FaPlayCircle/></p>
+        </div>
+    </div>
+
     </div>
   )
 }
